f3cfe1eb3d5971cf8bd41442d1ac7404c275bf81
BasmaBenNasr <bennasrbasma97@gmail.com>
2021-05-16 19:12:38 +0100

    creating the update and retrieve methods

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -33,12 +33,38 @@ exports.create=(req,res)=>{
 
 //retrieve and return all users/ retrieve nd reture a single user
 exports.find=(req,res)=>{
+    Userdb.find()
+    .then(user=>{
+        res.send(user)
+    })
+    .catch(err=>{
+        res.status(500).send({message:err.message || "We cannot find this user information"})
+    })
     
 }
 
 
 //update a new user by useer id
 exports.update=(req,res)=>{
+    if(!req.body){
+        return res
+        .status(400)
+        .send({message:"We cannot update empty data"})
+    }
+
+    const id = req.params.id
+    
+    Userdb.findByIdAndUpdate(id,req.body,{useFindAndModify:false})
+    .then(data=>{
+        if(!data){
+            res.status(404).send({message:`Cannot update user with ${id}, Maybe use not found!`})
+        }else{
+        res.send(data)
+    }
+    })
+    .catch(err=>{
+        res.status(500).send({message:"Error occured while updating user information"})
+    })
     
 }
 
