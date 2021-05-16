b2369169407125e3a75bdbd3832e8643c6e03ad9
BasmaBenNasr <bennasrbasma97@gmail.com>
2021-05-16 22:10:26 +0100

    last version completing all the functionalities

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,6 +23,29 @@ $("#update_user").submit(function(event){
         "data":data
     }
     $.ajax(request).done(function(request){
-        alert("Your Data is successfully Updated")
+        alert("Your Data is Successfully Updated")
     })
 })
+
+
+if(window.location.pathname == "/"){
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        var request = {
+            //im gonna use ajax to send the request to the server and get the response from the server
+            "url":`http://localhost:3000/api/users/${id}`,
+            "method":"DELETE",
+            
+        }
+
+        if(confirm("Are you sure you want to delete this record?")){
+            $.ajax(request).done(function(response){
+                alert("Your Data is Successfully Deleted!");
+                location.reload();
+            })
+        }
+
+    })
+}
