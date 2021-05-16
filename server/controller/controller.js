ed0649ea24a3dc07dba81a39f85ae8d07739c1b4
BasmaBenNasr <bennasrbasma97@gmail.com>
2021-05-16 18:39:04 +0100

    creating and saving the user in the database

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,8 +1,32 @@
 var Userdb= require('../model/model')
 
 //create and save new user
-
 exports.create=(req,res)=>{
+ //validate request
+ if(!req.body){
+     res.status(400).send({message: "Content cannot be empty!"});
+     return;
+ }
+
+ //new user
+ const user = new Userdb({
+     name:req.body.name,
+     email:req.body.email,
+     gender:req.body.gender,
+     status:req.body.status
+ })
+
+ // save the user in the database
+ user
+    .save(user)
+    .then(data=>{
+        res.send(data)
+    })
+    .catch(err=>{
+        res.status(500).send({
+            message: err.message || "Sorry something went wrong while creating a create operation"
+        })
+    })
 
 }
 
