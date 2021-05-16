72282274fa5dae60a6854511928b4cc3b2431841
BasmaBenNasr <bennasrbasma97@gmail.com>
2021-05-16 11:26:07 +0100

    creating modules

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const dotenv = require("dotenv")
 const morgan = require("morgan");
+const bodyparser = require("body-parser")
+const path = require("path")
+
+
+
 const app = express();
 
 dotenv.config({path:'config.env'})
@@ -9,6 +14,17 @@ const PORT=process.env.PORT || 5000
 //log requests
 app.use(morgan('tiny'));
 
+
+//parse request to body parser
+app.use(bodyparser.urlencoded({extended:true}));
+
+//set view engine
+app.set("view engine", "ejs");
+
+//load assets
+app.use("/css",express.static(path.resolve(__dirname,"assets/css")));
+app.use("/img",express.static(path.resolve(__dirname,"assets/img")));
+app.use("/js",express.static(path.resolve(__dirname,"assets/js")));
 
 app.get('/', (req,res)=>{
     res.send("Crud Application");
