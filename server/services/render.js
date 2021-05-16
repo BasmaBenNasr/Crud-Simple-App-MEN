824c9e98e10e37799bdbb76713256d8577293dad
BasmaBenNasr <bennasrbasma97@gmail.com>
2021-05-16 20:52:13 +0100

    adding the user inside the data base by creating a new user on the browser form

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,17 @@
+//import the axios library to allow us to render all the data on the browser
+
+const axios = require('axios')
+
+
 exports.homeRoutes=(req,res)=>{
-    res.render("index");
+    //i want to make a get request to the /api/users
+    axios.get('http://localhost:3000/api/users')
+    .then(function(response){
+        res.render('index',{users:response.data})
+    })
+    .catch(err=>{
+        res.send(err);
+    })
 }
 
 exports.add_user=(req,res)=>{
